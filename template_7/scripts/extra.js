cfd7db859a994e1cf22ebfb52d7cf65b7f0035d0
angelogic <jadecritx@gmail.com>
2021-12-23 18:18:09 -0500

    template sun/moon mode 12/23

---
+++ template_7/scripts/extra.js
@@ -0,0 +1,24 @@
+/*function toggleMode() {
+  document.getElementById("main").classList.toggle("night");
+}
+*/
+/*function toggleTheme() {
+            // Obtains an array of all <link>
+            // elements.
+            // Select your element using indexing.
+            var theme = document.getElementsByTagName('link')[0];
+  
+            // Change the value of href attribute 
+            // to change the css sheet.
+            if (theme.getAttribute('href') == 'dark.css') {
+                theme.setAttribute('href', 'style.css');
+            } else {
+                theme.setAttribute('href', 'dark.css');
+            }
+        }*/
+
+function myfunc() {
+   var element=document.getElementById("mybody");
+   element.classList.toggle("sunmood");
+   
+}
